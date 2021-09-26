f56ead889a5d5bc4b6e7383d0a0261a82443dfe1
Prasad Dananjaya Wilagama <85976286+PDWC@users.noreply.github.com>
2021-09-26 21:29:55 +0530

    JOB Management IT20195366
    There is not enough space so that add only the SRC file

---
+++ src/main/java/com/app/shopping/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.app.shopping.Prevalent;
+
+import com.app.shopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
